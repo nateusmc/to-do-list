341db40663e4f9e8066f54dca4e5a6fed6a4631f
Nate Garrett <nate.garrett1992@gmail.com>
2017-11-08 10:28:08 -0600

    got starter files setup

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+constructor() {
+  super()
+
+  this.state = {
+    todos: []
+  }
+  this.addToDo = this.addToDo.bind(this)
+  this.deleteToDo = this.deleteToDo.bind(this)
+}
+
+addToDo() {
+
+}
+
+deleteToDo() {
+
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Title />
+        <AddToList addtolist={this.addToDo}/>
+        <List />
       </div>
     );
   }
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,2 +1,2 @@
 import React from 'react'
-import './List.css'
+import './Title.css'
