417aa1fd08f15c54535e82bb4a0b043639fdd128
Nate Garrett <nate.garrett1992@gmail.com>
2017-11-08 11:48:44 -0600

    Got list working to populate and render the state when item is added to to-do list

--- src/components/List.js
+++ src/components/List.js
@@ -1,2 +1,18 @@
 import React from 'react'
 import './List.css'
+
+
+export default function List (props) {
+
+const todo = props.listOfToDo.map((task, index) => {
+return <li key={index}>{task}<button>delete</button></li>
+})
+
+  return (
+    <div>
+    <ul>
+    {todo}
+    </ul>
+    </div>
+  )
+}
